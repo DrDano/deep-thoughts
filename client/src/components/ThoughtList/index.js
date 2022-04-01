636bf003e82c448e92c5cbe4b1d6a5bdcd302c9c
DrDano <danielharned@gmail.com>
2022-03-31 21:29:11 -0400

    adding single thought query and page component

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../../utils/queries"
 
 const ThoughtList = ({ thoughts, title }) => {
+
   if (!thoughts.length) {
     return <h3>No Thoughts Yet</h3>;
   }
@@ -12,15 +16,23 @@ const ThoughtList = ({ thoughts, title }) => {
         thoughts.map((thought) => (
           <div key={thought._id} className="card mb-3">
             <p className="card-header">
-              {thought.username}
+              <Link
+                to={`/profile/${thought.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {thought.username}
+              </Link>{" "}
               thought on {thought.createdAt}
             </p>
             <div className="card-body">
-              <p>{thought.thoughtText}</p>
-              <p className="mb-0">
-                Reactions: {thought.reactionCount} || Click to{" "}
-                {thought.reactionCount ? "see" : "start"} the discussion!
-              </p>
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{" "}
+                  {thought.reactionCount ? "see" : "start"} the discussion!
+                </p>
+              </Link>
             </div>
           </div>
         ))}
