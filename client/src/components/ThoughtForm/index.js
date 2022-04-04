69c60b3a87a06047644a3a83f11a515b032dbd98
DrDano <danielharned@gmail.com>
2022-04-03 18:48:00 -0700

    adding thought submission form in new component ThoughtForm

---
+++ client/src/components/ThoughtForm/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const ThoughtForm = () => {
+  return (
+    <div>
+      <p className="m-0">Character Count: 0/280</p>
+      <form className="flex-row justify-center justify-space-between align-stretch">
+        <textarea
+          placeholder="Here's a new thought..."
+          className="form-input col-12 col-md-3"
+          type="submit"
+        ></textarea>
+        <button className="btn col-12 col-md-3" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
